1a10f377c926cc4d93c3f72df3bc1faa5b14f35f
Alban <alban.xhaferllari@gmail.com>
2019-11-24 19:06:51 -0600

    set up atlas account and connect to cloud db

---
+++ config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on('connected', function () {
+    console.log(`Mongoose connected to atlas DB`);
+  });

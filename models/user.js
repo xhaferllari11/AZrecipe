23a72bb31c1ccfd7f11c2ce64a84803e5865dc0d
Alban <alban.xhaferllari@gmail.com>
2019-11-27 13:04:45 -0600

    changed user model with sub schema of previously searched recipes

--- models/user.js
+++ models/user.js
@@ -2,12 +2,15 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 const userSchema = new schema({
-    googleId: {type: String},
-    name: {type: String},
-    profilePic: {type: String},
-    diet: {type: String},
-    email: {type: String},
-    recipes: [{
+    googleId: { type: String },
+    name: { type: String },
+    profilePic: { type: String },
+    email: { type: String },
+    oldRecipes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Recipe'
+    }],
+    currRecipes: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Recipe'
     }],
@@ -15,7 +18,21 @@ const userSchema = new schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Rating'
     }],
-    intolerances: [String]
-})
+    searched: [searchedSchema]
+}, {
+    timestamps: true
+});
+
+const searchedSchema = new schema({
+    cuisine: [{ type: String }],
+    diet: [{ type: String }],
+    intolerances: [{ type: String }],
+    mealType: [{ type: String }],
+    offset: { type: Number },
+    numResults: { type: Number }
+}, {
+    timestamps: true
+});
+
 
 module.exports = mongoose.model('User', userSchema)

91244a56fcdc1bdaa83020fdea4cb93acbb2c1c0
Alban <alban.xhaferllari@gmail.com>
2019-12-02 14:43:01 -0600

    able to show current recipes page with recipes, will need to go back and use promises instead of callbacks in creating recipes

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -9,7 +9,7 @@ const request = require('request');
 require('dotenv').config();
 
 
-const recipesPerCall = 2;
+const recipesPerCall = 1;
 const spBase = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.API_KEY}&number=${recipesPerCall}&instructionsRequired=true&fillIngredients=true&addRecipeInformation=true`
 
 
@@ -17,7 +17,16 @@ function showNew(req, res, next) {
     res.render('recipes/new', { u: req.user, e: null });
 }
 
+function index(req, res, next) {
+    User.findById(req.user._id)
+        .populate((req.params.currpre == 'current') ? 'currRecipes' : 'oldRecipes')
+        .exec(function (e, u) {
+            res.render('recipes/index', { u, page: req.params.currpre });
+        })
+}
+
 function getNewRecipes(req, res, next) {
+    //converts API request inputs to arrays
     let recipeReq = { diet: req.body.diet };
     recipeReq.cuisine = getArrayReq(req.body.cuisine);
     recipeReq.intolerances = getArrayReq(req.body.intolerances);
@@ -26,15 +35,14 @@ function getNewRecipes(req, res, next) {
     //checks if same request was made previously and updates offset
     //finds if similar search was made before and gives offset to update
     //currently user is not allowed to update search methods because finding
-    //old searches would take too many for loops. will update later
-
+    //old searches would take too many for loops. Will update later
     User.findById(req.user._id, function (e, u) {
         if (u.searches.length > 0) {
             recipeReq.offset = u.searches[0].offset
         } else {
             recipeReq.offset = 0;
         }
-
+        //converts search parameter to API string
         let APIReqURL = getReqURL(recipeReq);
         let reqOptions = { url: APIReqURL }
         request(reqOptions, function (err, response, body) {
@@ -42,9 +50,8 @@ function getNewRecipes(req, res, next) {
                 res.render('recipes/new', { u: req.user, e });
             };
             rawRecipes = JSON.parse(body);
+            //API returns a large object body and this function converts it to Schema
             recipes = convertToSchema(rawRecipes);
-
-            //command if new searches allowed: u.searches.push(recipeReq);
             //populte old recipes arr with recipes cooked last week
             u.oldRecipes = u.oldRecipes.concat(u.currRecipes);
             u.currRecipes = [];
@@ -53,35 +60,32 @@ function getNewRecipes(req, res, next) {
             recipes.forEach(function (recipe) {
                 //if recipe already in my db, change reference to that
                 //instead of saving new recipe
-                Recipe.findOne({ spoontacularId: recipe.spoontacularId })
-                    .exec(function (e, r) {
-                        if (r) {
-                            u.currRecipes.push(r);
-                        } else {
-                            let r = new Recipe(recipe);
-                            r.save(function (e, rec) {
-                                if (e) { console.log(111, e); };
-                                u.currRecipes.push(rec);
-                            });
-                        };
-                    });
+                Recipe.findOne({ spoontacularId: recipe.spoontacularId }, function (e, r) {
+                    if (r) {
+                        u.currRecipes.push(r);
+                    } else {
+                        let r = new Recipe(recipe);
+                        r.save(function (e, rec) {
+                            if (e) { console.log(111, e); };
+                            u.currRecipes.push(rec);
+                        });
+                    };
+                });
             });
             u.searches[0].offset = recipeReq.offset + rawRecipes.number;
-            console.log(u.searches.offset);
             if (u.searches.length < 1) { u.searches.push(recipeReq); };
-            console.log(1, recipes[0].id);
-            u.save();
+            //will need to change this to include promises, development only
+            setTimeout(()=> u.save(), 1000);
         });
     });
-
     res.redirect('/user/recipes/new');
 };
 
 module.exports = {
     showNew,
-    new: getNewRecipes
+    create: getNewRecipes,
+    index
 }
-
 
 
 //converts the form inputs into arrays as neccessary,
@@ -119,6 +123,7 @@ function getReqURL(reqField) {
     return reqURL;
 }
 
+//converts returned object recipe to schema type object
 function convertToSchema(rawR) {
     rawR.results.forEach(r => {
         r.spoontacularId = r.id;
--- routes/recipes.js
+++ routes/recipes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const recipeCtrl = require('../controllers/recipes');
 
 router.get('/new', isLoggedIn, recipeCtrl.showNew);
-router.post('/new', isLoggedIn, recipeCtrl.new);
+router.post('/new', isLoggedIn, recipeCtrl.create);
+
+router.get('/:currpre', isLoggedIn, recipeCtrl.index);
+
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();

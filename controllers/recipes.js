0abdac19e5e0acfb2ef2cb147242f23df068ce8d
Alban <alban.xhaferllari@gmail.com>
2019-12-04 14:16:11 -0600

    added to heroku, fixed new recipe page with for loops for better preferences population

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -9,13 +9,18 @@ const Rating = require('../models/rating');
 const request = require('request');
 require('dotenv').config();
 
-
+const meals = {
+    cuisines: ['African','American','British','Cajun','Caribbean','Chinese','Eastern European','European','French','German','Greek','Indian','Irish','Italian','Japanese','Jewish','Korean','Latin American','Mediterranean','Mexian','Middle Eastern','Spanish','Thai','Vietnamese'],
+    diet: ['Gluten Free','Ketogenic','Vegetarian','Vegan','Pescetarian','Paleo','Whole30'],
+    intolerances: ['Dairy','Egg','Gluten','Grain','Peanut','Seafood','Sesame','Shellfish','Soy','Sulfite','Tree nut','Wheat'],
+    mealType: ['Main Course','Soup','Dessert']
+}
 const recipesPerCall = 1;
 const spBase = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.API_KEY}&number=${recipesPerCall}&instructionsRequired=true&fillIngredients=true&addRecipeInformation=true`
 
 
 function showNew(req, res, next) {
-    res.render('recipes/new', { u: req.user, e: null });
+    res.render('recipes/new', { u: req.user, e: null, m: meals });
 }
 
 function index(req, res, next) {
@@ -30,10 +35,10 @@ function index(req, res, next) {
 function show(req, res, next) {
     let fieldHandler = (req.params.currpre == 'current') ? 'currRecipes' : 'oldRecipes';
     Recipe.findById(req.params.recId, function (e, r) {
-        User.findById(req.user._id, function(e,u){
+        User.findById(req.user._id, function (e, u) {
             ratingId = r.ratings.filter(element => u.ratings.includes(element));
             if (ratingId.length) {
-                Rating.findById(ratingId[0], function(e,rat){
+                Rating.findById(ratingId[0], function (e, rat) {
                     res.render('recipes/show', { u, page: fieldHandler, r, rat });
                 });
             } else {
@@ -116,21 +121,23 @@ function show(req, res, next) {
 
 function getNewRecipes(req, res, next) {
     //converts API request inputs to arrays
-    let recipeReq = { diet: req.body.diet };
-    recipeReq.cuisine = getArrayReq(req.body.cuisine);
-    recipeReq.intolerances = getArrayReq(req.body.intolerances);
-    recipeReq.mealType = getArrayReq(req.body.meal);
-
-    //checks if same request was made previously and updates offset
-    //finds if similar search was made before and gives offset to update
-    //currently user is not allowed to update search methods because finding
-    //old searches would take too many for loops. Will update later
     User.findById(req.user._id, function (e, u) {
+        let recipeReq = {};
+
+        //checks if same request was made previously and updates offset
+        //finds if similar search was made before and gives offset to update
+        //currently user is not allowed to update search methods because finding
+        //old searches would take too many for loops. Will update later
         if (u.searches.length > 0) {
-            recipeReq.offset = u.searches[0].offset
+            recipeReq = u.searches[0];
         } else {
             recipeReq.offset = 0;
+            recipeReq.diet = req.body.diet;
+            recipeReq.cuisine = getArrayReq(req.body.cuisine);
+            recipeReq.intolerances = getArrayReq(req.body.intolerances);
+            recipeReq.mealType = getArrayReq(req.body.meal);
         }
+
         //converts search parameter to API string
         let APIReqURL = getReqURL(recipeReq);
         let reqOptions = { url: APIReqURL }
@@ -147,33 +154,33 @@ function getNewRecipes(req, res, next) {
 
             let recPromises = [];
             //saves each recipe to database
-            for (let i=0;i<recipes.length;i++){
+            for (let i = 0; i < recipes.length; i++) {
                 //if recipe already in my db, change reference to that
                 //instead of saving new recipe
-                recPromises.push(Recipe.findOne({spoonacularId: recipes[i].spoonacularId}));
+                recPromises.push(Recipe.findOne({ spoonacularId: recipes[i].spoonacularId }));
             }
             Promise.all(recPromises)
-            .then(function(results){
-                let newRecPromises = []
-                for (let i=0;i<results.length;i++){
-                    if (results[i]){
-                        u.currRecipes.push(results[i]);
-                    } else {
-                        let r = new Recipe(recipes[i]);
-                        newRecPromises.push(r.save());
+                .then(function (results) {
+                    let newRecPromises = []
+                    for (let i = 0; i < results.length; i++) {
+                        if (results[i]) {
+                            u.currRecipes.push(results[i]);
+                        } else {
+                            let r = new Recipe(recipes[i]);
+                            newRecPromises.push(r.save());
+                        }
                     }
-                }
-                return Promise.all(newRecPromises);
-            }).then(function(results){
-                results.forEach(function(result){
-                    u.currRecipes.push(result);
+                    return Promise.all(newRecPromises);
+                }).then(function (results) {
+                    results.forEach(function (result) {
+                        u.currRecipes.push(result);
+                    });
+                    if (u.searches.length < 1) { u.searches.push(recipeReq); };
+                    u.searches[0].offset = recipeReq.offset + rawRecipes.number;
+                    return u.save();
+                }).then(function (result) {
+                    res.redirect('/user/recipes/current');
                 });
-                u.searches[0].offset = recipeReq.offset + rawRecipes.number;
-                if (u.searches.length < 1) { u.searches.push(recipeReq); };
-                return u.save();
-            }).then(function(result){
-                res.redirect('/user/recipes/current');
-            });
         });
     });
 };
@@ -210,14 +217,17 @@ function getReqURL(reqField) {
             reqURL = `${reqURL}&diet=${reqField['diet']}`
         } else if (option === 'offset') {
             reqURL = `${reqURL}&offset=${reqField.offset}`
-        } else if (reqField[option].length) {
-            reqURL = `${reqURL}&${option}=`
-            for (let i = 0; i < reqField[option].length; i++) {
-                reqURL = `${reqURL}${reqField[option][i]}`
-                if (i !== reqField[option].length - 1) {
-                    reqURL = reqURL + ',';
+        } else if (option == 'cuisine' || 
+        option == 'intolerances' || option == 'mealType') {
+            if (reqField[option].length) {
+                reqURL = `${reqURL}&${option}=`
+                for (let i = 0; i < reqField[option].length; i++) {
+                    reqURL = `${reqURL}${reqField[option][i]}`
+                    if (i !== reqField[option].length - 1) {
+                        reqURL = reqURL + ',';
+                    }
                 }
-            }
+            } 
         }
     }
     return reqURL;
